c6ce2c1b7f883da19a6c23962551d1d165412be6
Noah Sehman <noah.sehman@gmail.com>
2024-06-02 08:50:08 -0700

    just slowly making things better. you know how it is!

--- Main/server/routes/userRoutes.js
+++ Main/server/routes/userRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
-const auth = require('../middleware/auth');
+const auth = require('@babel/middleware/auth');
 
 const router = express.Router();
 
