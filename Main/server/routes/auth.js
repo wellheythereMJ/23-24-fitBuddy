6b924593a2440779184a034907833fba83a236cc
Noah Sehman <noah.sehman@gmail.com>
2024-06-02 13:43:04 -0700

    also fixed this. I'm so good at tech

--- Main/server/routes/auth.js
+++ Main/server/routes/auth.js
@@ -1,19 +1,20 @@
 const express = require('express');
 const argon2 = require('argon2');
+const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
 const router = express.Router();
 
 router.post('/signup', async (req, res) => {
-  const { username, password } = req.body;
-  try {
-    const hashedPassword = await argon2.hash(password);
-    const newUser = new User({ username, password: hashedPassword });
-    await newUser.save();
-    res.status(201).json({ message: 'User created successfully' });
-  } catch (error) {
-    res.status(500).json({ error: 'Internal server error' });
-  }
+    const { username, password } = req.body;
+    try {
+      const hashedPassword = await argon2.hash(password);
+      const newUser = new User({ username, password: hashedPassword });
+      await newUser.save();
+      res.status(201).json({ message: 'User created successfully' });
+    } catch (error) {
+      res.status(500).json({ error: 'Internal server error' });
+    }
 });
 
 router.post('/login', async (req, res) => {
--- Main/server/server.js
+++ Main/server/server.js
@@ -1,32 +1,21 @@
-// server/server.js
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors');
 const userRoutes = require('./routes/userRoutes');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 
-// Middleware
-app.use(cors());
+mongoose.connect('mongodb://localhost:27017/yourdatabase', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.log(err));
+
 app.use(express.json());
 
-// MongoDB Connection
-mongoose.connect('mongodb://localhost:27017/fitbuddy', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => {
-  console.log('Connected to MongoDB');
-}).catch((error) => {
-  console.error('Connection error', error.message);
-});
-
-app.use('/api/users', userRoutes);
-
-app.get('/', (req, res) => {
-  res.send('Hello from HorsePower server');
-});
+app.use('/api/auth', userRoutes);
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
