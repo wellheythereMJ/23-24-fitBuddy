682b2adbbcc68cc9aff9a5cf93243353d34f3c26
Noah Sehman <noah.sehman@gmail.com>
2024-06-03 21:31:33 -0700

    baby steps

--- Main/server/server.js
+++ Main/server/server.js
@@ -1,21 +1,33 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
 const mongoose = require('mongoose');
-const userRoutes = require('./routes/userRoutes');
+const cors = require('cors');
+
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 mongoose.connect('mongodb://localhost:27017/yourdatabase', {
   useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.log(err));
+  useUnifiedTopology: true,
+}).then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
 
+app.use(cors());
 app.use(express.json());
 
-app.use('/api/auth', userRoutes);
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
+
+await server.start();
+server.applyMiddleware({ app });
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  console.log(`GraphQL endpoint at http://localhost:${PORT}${server.graphqlPath}`);
 });
